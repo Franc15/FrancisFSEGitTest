aedf1aee615d78c573e06d9ff8aa6e3d1801177e
Francis Kikulwe <francissteven1@gmail.com>
2021-11-02 11:52:12 +0400

    Person class first commit

---
+++ Person.java
@@ -0,0 +1,18 @@
+public class Person
+{
+	String firstName;
+	String surname;
+
+	public Person()
+	{
+		this.firstName = "DEFAULT";
+		this.surName = "DEFAULT";
+	}
+
+	public Person(String firstName, String surName)
+	{
+		this.firstName = firstName;
+		this.surName = surName;
+	}
+}
+
